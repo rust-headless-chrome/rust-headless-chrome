ed21920f0ac8e9531f42ee9215a0b28dfa3d16bd
Alistair Roche <roche.a@gmail.com>
2019-02-17 10:46:17 +1100

    Make most modules not public

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,17 @@ extern crate termcolor;
 
 pub mod browser;
 pub mod cdtp;
-pub mod element;
-pub mod helpers;
-pub mod keys;
-pub mod logging;
-pub mod point;
-pub mod process;
-pub mod tab;
-pub mod transport;
-pub mod waiting_call_registry;
-pub mod web_socket_connection;
+mod tab;
+mod point;
+mod process;
+mod transport;
+mod waiting_call_registry;
+mod web_socket_connection;
+mod element;
+mod helpers;
+mod keys;
+
+#[cfg(test)]
+mod logging;
 
 pub use browser::{Browser, LaunchOptions};
