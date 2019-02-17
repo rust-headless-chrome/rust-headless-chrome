377506f04a558d92d540bf986eb246c95ad24463
Alistair Roche <roche.a@gmail.com>
2019-02-17 12:34:41 +1100

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@ extern crate termcolor;
 
 pub mod browser;
 pub mod cdtp;
-mod tab;
-mod point;
-mod process;
-mod transport;
-mod waiting_call_registry;
-mod web_socket_connection;
 mod element;
 mod helpers;
 mod keys;
+mod point;
+mod process;
+mod tab;
+mod transport;
+mod waiting_call_registry;
+mod web_socket_connection;
 
 #[cfg(test)]
 mod logging;
