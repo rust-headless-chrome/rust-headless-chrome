af74783e099c48d06acbd59972099565cb56c47b
Alistair Roche <roche.a@gmail.com>
2019-02-19 11:39:48 +1100

    Try adding flags from https://github.com/GoogleChrome/chrome-launcher/blob/master/src/flags.ts

--- src/browser/process.rs
+++ src/browser/process.rs
@@ -115,6 +115,28 @@ impl LaunchOptions {
     }
 }
 
+const DISABLE_SERVICES_FLAGS: [&'static str; 9] = [
+    // Disable built-in Google Translate service
+    "--disable-translate",
+    // Disable all chrome extensions entirely
+    "--disable-extensions",
+    // Disable various background network services, including extension updating,
+    //   safe browsing service, upgrade detector, translate, UMA
+    "--disable-background-networking",
+    // Disable fetching safebrowsing lists, likely redundant due to disable-background-networking
+    "--safebrowsing-disable-auto-update",
+    // Disable syncing to a Google account
+    "--disable-sync",
+    // Disable reporting to UMA, but allows for collection
+    "--metrics-recording-only",
+    // Disable installation of default apps on first run
+    "--disable-default-apps",
+    // Mute any audio
+    "--mute-audio",
+    // Skip first run wizards
+    "--no-first-run",
+];
+
 impl Process {
     pub fn new(launch_options: LaunchOptions) -> Result<Self, Error> {
         info!("Trying to start Chrome");
@@ -182,6 +204,8 @@ impl Process {
             data_dir_option.as_str(),
             //            "--window-size=1920,1080"
         ];
+
+        args.extend_from_slice(&DISABLE_SERVICES_FLAGS);
 
         if launch_options.headless {
             args.extend(&["--headless"]);
