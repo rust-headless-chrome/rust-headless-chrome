bb89cb5c8e7a30699c81b1aaaa0b4cf4ba87d661
Alistair Roche <roche.a@gmail.com>
2019-02-19 15:42:24 +1100

    Fix imports in screenshot example

--- examples/take_screenshot.rs
+++ examples/take_screenshot.rs
@@ -1,11 +1,13 @@
 //! Create a headless browser, navigate to wikipedia, wait for the page
 //! to render completely, take a screenshot in JPEG-format using 75% quality
-use headless_chrome::{cdtp::page::ScreenshotFormat, Browser, LaunchOptions};
+use headless_chrome::{cdtp::page::ScreenshotFormat, Browser, LaunchOptionsBuilder};
 use std::fs;
 
 fn main() -> Result<(), failure::Error> {
-    let opts = LaunchOptions::default().expect("Could not find chrome");
-    let browser = Browser::new(opts)?;
+    let options = LaunchOptionsBuilder::default()
+        .build()
+        .expect("Couldn't find appropriate Chrome binary.");
+    let browser = Browser::new(options).expect("Failed to launch and connect to Chrome");
     let jpeg_data = browser
         .wait_for_initial_tab()?
         .navigate_to("https://www.wikipedia.org")?
