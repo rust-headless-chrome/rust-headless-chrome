555f09f89e5a96e7f374e77d71b1d1ccff91f81f
Evan Cameron <cameron.evan@gmail.com>
2019-02-22 19:34:11 -0500

    handle archive name, path creation, download

--- src/browser/revision.rs
+++ src/browser/revision.rs
@@ -1,10 +1,16 @@
+use directories::ProjectDirs;
 use failure::Error;
 use log::*;
 use reqwest;
-use std::path::Path;
-use xdg;
+
+use std::{
+    fs::File,
+    io,
+    path::{Path, PathBuf},
+};
 
 const APP_NAME: &str = "headless-chrome";
+const DEFAULT_HOST: &str = "https://storage.googleapis.com";
 
 const REV_URL: &str =
     "https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/LAST_CHANGE";
@@ -14,6 +20,22 @@ const REV_URL: &str = "https://storage.googleapis.com/chromium-browser-snapshots
 const REV_URL: &str =
     "https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/LAST_CHANGE";
 
+pub fn download<U, P>(url: U, path: P) -> Result<(), Error>
+where
+    U: AsRef<str>,
+    P: AsRef<Path>,
+{
+    info!("Downloading file from: {}", url.as_ref());
+    let mut resp = reqwest::get(url.as_ref())?;
+
+    info!("Creating dir: {}", path.as_ref().display());
+    let mut dest = File::create(path.as_ref())?;
+
+    info!("Copying data into location");
+    io::copy(&mut resp, &mut dest)?;
+    Ok(())
+}
+
 pub fn get_latest_rev() -> Result<String, Error> {
     info!("Getting latest chrome revision");
     let rev = reqwest::get(REV_URL)?.text()?;
@@ -21,7 +43,53 @@ pub fn get_latest_rev() -> Result<String, Error> {
     Ok(rev)
 }
 
-pub fn get_xdg_dir() -> Result<impl AsRef<Path>, Error> {
-    let xdg_dir = xdg::BaseDirectories::with_prefix(APP_NAME)?;
-    Ok(xdg_dir.get_data_home().as_path())
+pub fn get_project_dirs() -> Result<ProjectDirs, String> {
+    info!("Getting project dir");
+    match ProjectDirs::from("", "", APP_NAME) {
+        Some(dirs) => Ok(dirs),
+        None => Err("Failed to retrieve project dirs".to_string()),
+    }
+}
+
+pub const fn dl_url<R>(revision: R) -> String
+where
+    R: AsRef<str>,
+{
+    return format!(
+        "{}/chromium-browser-snapshots/Linux_x64/{}/{}.zip",
+        DEFAULT_HOST,
+        revision.as_ref(),
+        archive_name(revision)
+    );
+
+    #[cfg(target_os = "macos")]
+    return format!(
+        "{}/chromium-browser-snapshots/Mac/{}/{}.zip",
+        DEFAULT_HOST,
+        revision.as_ref(),
+        archive_name(revision)
+    );
+
+    #[cfg(windows)]
+    return format!(
+        "{}/chromium-browser-snapshots/Win_x64/{}/{}.zip",
+        DEFAULT_HOST,
+        revision.as_ref(),
+        archive_name(revision)
+    );
+}
+
+pub const fn archive_name<S: AsRef<str>>(revision: S) -> &'static str {
+    #[cfg(target_os = "macos")]
+    return "chrome-mac";
+
+    #[cfg(windows)]
+    // Windows archive name changed at r591479.
+    return if revision.as_ref().parse::<u32>()? > 591479 {
+        "chrome-win"
+    } else {
+        "chrome-win32"
+    };
+
+    "chrome-linux"
 }
