8308a55ef8c97544450936afc714f7e5d648a53a
DudeTux42 <lindermayr@pm.me>
2024-11-18 11:51:19 +0100

    changed the example code in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,43 +16,63 @@
 //! # Quick Start
 //!
 //! ```no_run
-//! use headless_chrome::{Browser, protocol::page::ScreenshotFormat};
-//! use headless_chrome::protocol::cdp::Page;
+//!use std::error::Error;
+//!use headless_chrome::Browser;
+//!use headless_chrome::protocol::cdp::Page;
 //!
-//! fn browse_wikipedia() -> Result<(), failure::Error> {
-//!     let browser = Browser::default()?;
+//!fn browse_wikipedia() -> Result<(), Box<dyn Error>> {
+//!    let browser = Browser::default()?;
 //!
-//!     let tab = browser.new_tab()?;
+//!    let tab = browser.new_tab()?;
 //!
-//!     /// Navigate to wikipedia
-//!     tab.navigate_to("https://www.wikipedia.org")?;
+//!    // Navigate to wikipedia
+//!    tab.navigate_to("https://www.wikipedia.org")?;
 //!
-//!     /// Wait for network/javascript/dom to make the search-box available
-//!     /// and click it.
-//!     tab.wait_for_element("input#searchInput")?.click()?;
+//!    // Wait for network/javascript/dom to make the search-box available
+//!    // and click it.
+//!    tab.wait_for_element("input#searchInput")?.click()?;
 //!
-//!     /// Type in a query and press `Enter`
-//!     tab.type_str("WebKit")?.press_key("Enter")?;
+//!    // Type in a query and press `Enter`
+//!    tab.type_str("WebKit")?.press_key("Enter")?;
 //!
-//!     /// We should end up on the WebKit-page once navigated
-//!     tab.wait_for_element("#firstHeading")?;
-//!     assert!(tab.get_url().ends_with("WebKit"));
+//!    // We should end up on the WebKit-page once navigated
+//!    let elem = tab.wait_for_element("#firstHeading")?;
+//!    assert!(tab.get_url().ends_with("WebKit"));
 //!
-//!     /// Take a screenshot of the entire browser window
-//!     let _jpeg_data = tab.capture_screenshot(
-//!         Page::CaptureScreenshotFormatOption::Png,
-//!         Some(75),
-//!         None,
-//!         true)?;
+//!    /// Take a screenshot of the entire browser window
+//!    let jpeg_data = tab.capture_screenshot(
+//!        Page::CaptureScreenshotFormatOption::Jpeg,
+//!        None,
+//!        None,
+//!        true)?;
+//!    // Save the screenshot to disc
+//!    std::fs::write("screenshot.jpeg", jpeg_data)?;
 //!
-//!     /// Take a screenshot of just the WebKit-Infobox
-//!     let _png_data = tab
-//!         .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
-//!         .capture_screenshot(ScreenshotFormat::PNG)?;
-//!     Ok(())
-//! }
+//!    /// Take a screenshot of just the WebKit-Infobox
+//!    let png_data = tab
+//!        .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
+//!        .capture_screenshot(Page::CaptureScreenshotFormatOption::Png)?;
+//!    // Save the screenshot to disc
+//!    std::fs::write("screenshot.png", png_data)?;
 //!
-//! assert!(browse_wikipedia().is_ok());
+//!    // Run JavaScript in the page
+//!    let remote_object = elem.call_js_fn(r#"
+//!        function getIdTwice () {
+//!            // `this` is always the element that you called `call_js_fn` on
+//!            const id = this.id;
+//!            return id + id;
+//!        }
+//!    "#, vec![], false)?;
+//!    match remote_object.value {
+//!        Some(returned_string) => {
+//!            dbg!(&returned_string);
+//!            assert_eq!(returned_string, "firstHeadingfirstHeading".to_string());
+//!        }
+//!        _ => unreachable!()
+//!    };
+//!
+//!    Ok(())
+//!}
 //! ```
 
 #![deny(clippy::pedantic)]
